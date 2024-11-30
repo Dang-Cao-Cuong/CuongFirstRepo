b8536ed252b97880b59c0d84e1c375305a71afaa
Dang-Cao-Cuong <cuong.dangcao.cit22@eiu.edu.vn>
2024-11-30 13:05:08 +0700

    Update print-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Cuong’s Java Repository.");
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        System.out.println("Hello "+input);
+        System.out.println("Welcome to my Repository.");
     }
 }
